42eb592b0386fa656adfd821b4ad94597dc42a82
Sonu Kumar Yadav <sonukryadav56@gmail.com>
2023-06-24 01:01:08 +0530

    i/p or o/p

--- 5scanf.c
+++ 5scanf.c
@@ -3,4 +3,11 @@ int main(){
     char ch;
     scanf("%c",&ch); // & called address of
     printf("%d\t %c", ch, ch);
+
+    // using getchar, putchar and scanf together
+    char ch1;
+    ch1 = getchar();
+    putchar(ch1);
+    scanf("\t%d", putchar(ch1));
+    return 0;
 }
