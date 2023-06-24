5d60a13a08b3a998757c37ff4a8b9def051daadc
Sonu Kumar Yadav <sonukryadav56@gmail.com>
2023-06-24 15:39:01 +0530

    scanf conversion specification

--- 5scanf.c
+++ 5scanf.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 int main(){
-    char ch;
-    scanf("%c",&ch); // & called address of
-    printf("%d\t %c", ch, ch);
-
     // using getchar, putchar and scanf together
     char ch1;
     ch1 = getchar();
     putchar(ch1);
-    scanf("\t%d", putchar(ch1));
+    printf("\t%d", putchar(ch1));
+    printf("\n");
+    //input:  a
+    //output: aa    97
+
+    char name[20];
+    int age;
+    float percentage;
+    printf("Enter your name, age and percentage : ");
+    scanf("%s%d%f", name, &age, &percentage);
+    printf("name : %s, age : %d, percentage : %f", name, age, percentage);
     return 0;
 }
